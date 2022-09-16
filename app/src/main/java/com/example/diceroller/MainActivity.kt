d57dea237dd82b2d3fc65889062f81bcbb9854d0
nathan <nathanstickler@gmail.com>
2022-09-16 10:20:12 -0700

    Updated app to use dataBinding instead of findViewById

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,9 +6,13 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.diceroller.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     // Want to minimize calls to findViewById as much as possible so extract
     // lateinit allows android to declare and initialize non null variables in on create
@@ -16,12 +20,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val rollButton = findViewById<Button>(R.id.roll_button)
-        rollButton.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        diceImage = binding.diceImage
+        binding.rollButton.setOnClickListener {
             rollDice()
         }
-        diceImage = findViewById(R.id.dice_image)
+//        setContentView(R.layout.activity_main)
+//        val rollButton = findViewById<Button>(R.id.roll_button)
+//        rollButton.setOnClickListener {
+//            rollDice()
+//        }
+//        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
